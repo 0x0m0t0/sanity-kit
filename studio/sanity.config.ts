2a8ce508e60921f80c284e52ec863eae7c6b0114
noodles4u <louisdesmett@gmail.com>
2024-01-11 20:55:18 +0100

    fixes

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
 import {structure} from './desk'
+// import {structureTool} from 'sanity/structure'  // !!! DEVCHECK
 
 export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
 export const dataset = process.env.SANITY_STUDIO_DATASET!
