7154c4ef28e88dac33611527dfd46e2044af9462
noodles4u <louisdesmett@gmail.com>
2024-01-18 16:11:19 +0100

    changes

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     media(),
     documentInternationalization({
       supportedLanguages: i18n.languages,
-      schemaTypes: ['work'],
+      schemaTypes: ['blog'],
     }),
     // !!! FORDEV
     schemaVisualizer({
