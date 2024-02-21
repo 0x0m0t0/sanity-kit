2198d2dfbc3ba051bf18bf9a0e85d59be8e1d730
noodles4u <louisdesmett@gmail.com>
2024-02-21 20:51:57 +0100

    preload test

--- app/src/routes/[[lang]]/+page.svelte
+++ app/src/routes/[[lang]]/+page.svelte
@@ -1,15 +1,28 @@
+<script context="module" lang="ts">
+	import type { PageData } from './$types';
+
+	export async function load({ fetch }) {
+		const res = await fetch('your-api-url');
+		const data: PageData = await res.json();
+
+		if (data.posts.length > 0) {
+			const link = document.createElement('link');
+			link.rel = 'preload';
+			link.as = 'image';
+			link.href = data.posts[0].mainImage;
+			document.head.appendChild(link);
+		}
+
+		return { props: { data } };
+	}
+</script>
+
 <script lang="ts">
 	import Card from '$components/Card.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
-
-<svelte:head>
-	{#if data.posts.length && data.posts[0].mainImage}
-		<link rel="preload" as="image" href={data.posts[0].mainImage} />
-	{/if}
-</svelte:head>
 
 <section>
 	<h1 class="sr-only">Home</h1>
